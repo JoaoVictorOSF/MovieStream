f25dceb52a50fb209367913b61c2731f0314f54d
João Victor VS <jv_gs@hotmail.com>
2021-03-01 05:42:51 -0300

    Problemas no HideMENU

--- src/app/shared/header/hide-menu/hide-menu.component.ts
+++ src/app/shared/header/hide-menu/hide-menu.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild  } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-hide-menu',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HideMenuComponent implements OnInit {
 
-  constructor() { }
+  constructor( private route: Router) { }
 
   ngOnInit(): void {
   }
-
+  goHome(){
+    this.route.navigate([`/`])
+  }
 }
